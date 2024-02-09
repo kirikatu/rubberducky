6081fd21ba57bcd53527f0895ee043aa725296d0
kirikatu <159580335+kirikatu@users.noreply.github.com>
2024-02-09 22:00:15 +0100

    Update Matrix.ps1

--- Matrix.ps1
+++ Matrix.ps1
@@ -1,1 +1,10 @@
-Write-host "test"
+$batchContent = @"
+@echo off
+color 2
+:top
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto top
+"@
+$batchFilePath = "c:\temp\matrix.bat"
+Set-Content -Path $batchFilePath -Value $batchContent
+Start-Process -FilePath $batchFilePath

